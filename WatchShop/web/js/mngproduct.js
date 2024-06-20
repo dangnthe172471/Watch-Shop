05a4e723bda778dbee69e842319805445dc78b8e
dangnthe172471 <dangnthe172471@fpt.edu.vn>
2024-06-21 03:33:21 +0700

    update product

---
+++ WatchShop/web/js/mngproduct.js
@@ -0,0 +1,22 @@
+// Chờ cho tài liệu HTML (DOM) được tải hoàn toàn
+document.addEventListener('DOMContentLoaded', function () {
+    // Lấy danh sách các ảnh thumbnail
+    const thumbImages = document.querySelectorAll('.thumb-img');
+
+    // Lặp qua từng ảnh thumbnail để thêm sự kiện click
+    thumbImages.forEach(img => {
+        img.addEventListener('click', function () {
+            // Lấy phần tử cha gần nhất chứa ảnh lớn của sản phẩm
+            const khoianhDiv = this.closest('.khoianh');
+
+            // Tìm phần tử ảnh lớn của sản phẩm trong phần tử cha
+            const mainImage = khoianhDiv.querySelector('.product-image');
+
+            // Lấy đường dẫn của ảnh thumbnail được click
+            const newSrc = this.getAttribute('src');
+
+            // Đổi src của ảnh lớn của sản phẩm thành đường dẫn của ảnh thumbnail được click
+            mainImage.src = newSrc;
+        });
+    });
+});
